81fed60afd7336c8efdb7d8f7b49c5757091d216
szcarr <itwangzy@gmail.com>
2022-02-16 08:27:41 +0100

    New version

--- src/path.py
+++ src/path.py
@@ -1,19 +1,34 @@
+from email.mime import base
 import fileHandling as fh
 import general as g
 
 obstacletxt = fh.getPathToCurrentDir("") + "obstacle.txt"
 print(obstacletxt)
+
 coordinates = {
 
 }
 
+baseCostOfMove = 10
+costOfMoving = {
+    0: baseCostOfMove,
+    1: baseCostOfMove * 100,
+}
+
+def algo():
+    while True:
+        pass
+        break
+
+
 def convertCoordinateToInt(coordinates):
+
     '''
     <coordinate> 'x y'
     '''
 
     lst = str(coordinates).split(" ")
-    return lst[0], lst[1]
+    return int(lst[0]), int(lst[1])
 
 def convertObstacleToCoordinates():
     output = fh.readTXTFile(obstacletxt)
@@ -21,7 +36,7 @@ def convertObstacleToCoordinates():
     global coordinates
     for y in range(len(output)):
         line = output[y]
-        print(line)
+        #print(line)
         tempList = line.split("\n")
         line = g.convertListToString(tempList)
         valueForEachSplit = line.split(" ")
@@ -33,20 +48,66 @@ def convertObstacleToCoordinates():
 def getRelativeDirection(a, b):
 
     '''
-    a should be 
+    a should be one set of coordinates
+    b should be different set of coordinates
     '''
     
-    if b > a:
-        pass
+    bX = convertCoordinateToInt(b)[0]
+    bY = convertCoordinateToInt(b)[1]
 
+    aX = convertCoordinateToInt(a)[0]
+    aY = convertCoordinateToInt(a)[1]
 
+    rightOfA = False
+    leftOfA = False
+    if bX > aX: #MEANS THAT b is to the right for a
+        rightOfA = True
+    elif bX < aX:
+        leftOfA = True
 
+    belowA = False
+    aboveA = False
 
-def getLocationByValue(value):
-    convertObstacleToCoordinates()
+    if bY > aY: #MEANS THAT b is to the right for a
+        belowA = True
+    elif bY < aY:
+        aboveA = True
+
+    return aboveA, rightOfA, belowA, leftOfA
+
+def getCoordinatesByValue(value):
+
+    '''
+    Returns coordinates in format "x y"
+    '''
+
     for key in coordinates.keys():
         if coordinates.get(key) == value:
             return key
 
+def getSurroundingValues(pos):
+    global coordinates
+    offSetList = [-1, 0, 1]
 
-print(getLocationByValue("Y"))
+    BASEX = convertCoordinateToInt(pos)[0]
+    BASEY = convertCoordinateToInt(pos)[1]
+
+    surroundingValues = []    
+    for y in range(len(offSetList)):
+        for x in range(len(offSetList)):
+            try:
+                #print("heri")
+                xpos = BASEX + offSetList[x]
+                ypos = BASEY + offSetList[y]
+                #print(offSetList[x], offSetList[y])
+                coords = str(xpos) + " " + str(ypos)
+                #print(coords, result)
+                surroundingValues.append(coordinates.get(coords))
+            except:
+                print("error")
+                pass
+
+    return surroundingValues
+
+convertObstacleToCoordinates() # MUST BE RUN FIRST
+getSurroundingValues(getCoordinatesByValue("X"))
